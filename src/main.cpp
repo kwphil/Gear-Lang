e85cd9d4347dc7c99c3fadd8dbf463fb19b53b3f
kwphil <phillipsw1980@gmail.com>
2025-04-09 08:54:57 -0500

    Working on the file reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char** argv) {
+    std::string file_input;   
+
+    // Make sure the file was included
+    if(argc < 2) {
+        std::cout << "Incorrect usage" << std::endl;
+        return 1;
+    }
+
+    // Open the file
+    std::ifstream file(argv[1]);
+
+    // Make sure the file actually exists
+    if(!file.is_open()) {
+        std::cout << "File: " << argv[1] << " is not found" << std::endl;
+        return 2;
+    }
+
+    // Then we can read it
+    {
+        // Getting the buf out of scope as soon as possible
+        std::stringstream buf;
+        buf << file.rdbuf();
+        file_input = buf;
+    }
+
+    std::cout << file_input;
 
     return EXIT_SUCCESS;
 }
